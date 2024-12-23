68b94057c421f6b8c84ef590aeeb79ce8f6746ae
GiorgioRossi2601 <grossi2601@gmail.com>
2024-12-23 15:34:12 +0100

    Modifica della NavigationView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.insubria_survive"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.insubria_survive"
